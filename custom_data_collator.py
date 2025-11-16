620e5ce928c85eece99a89e030011d749f0d624e
hengyu-wang <why3065619030@163.com>
2025-11-16 22:45:49 +0800

    transformers

--- custom_data_collator.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Literal, Optional
-
-import numpy as np
-import torch
-import torch.nn.functional as F
-from transformers import DataCollatorForSeq2Seq
